45d0fd5c60ef26db68a734771dfa0647e724cd1e
Tsvi Zandany <szandany@liquibase.com>
2021-11-14 17:19:08 -0600

    changeLog updated

--- changelog.sql
+++ changelog.sql
@@ -1,4 +1,4 @@
--- liquibase formatted sql
+-- liquibase formatted sql changeLogId:8e1960a5-1a32-416f-b97d-44e8a91f973e
 
 -- Changeset SteveZ:createTable-MYTABLE5 context:DEV labels:Feature1
 CREATE TABLE MYTABLE5 (C1 STRING, C2 STRING);
